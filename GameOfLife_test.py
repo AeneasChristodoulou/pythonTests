e6ef7ac2047074857b6414abcb335c7a157b2aba
aeneas <streamcloud20@gmail.com>
2022-03-07 20:02:57 +0100

    Updating GameOfLife

--- GameOfLife_test.py
+++ GameOfLife_test.py
@@ -1,6 +1,9 @@
+from copy import deepcopy
 from typing import List
 
-def create_dead_cells(x :int =2, y :int =2) -> List[List[int]]:
+
+Grid = List[List[int]]
+def create_dead_cells(x :int =2, y :int =2) -> Grid:
     grid_y = []
     for row_number in range(0, y):
         grid_y.append(0)
@@ -13,7 +16,31 @@ def create_dead_cells(x :int =2, y :int =2) -> List[List[int]]:
 
 
 
+
 def test_create_grid():
     assert create_dead_cells(2, 5) == [ [0, 0, 0, 0, 0], [0, 0, 0, 0, 0] ]
     assert create_dead_cells(1, 2) == [ [0, 0] ]
     assert create_dead_cells(2, 2) == [ [0, 0], [0, 0] ]
+
+def evolutionary_grid_generator(grid: Grid) -> Grid:
+
+    result = deepcopy(grid)
+    for position_lines, a_line in enumerate(grid):
+        for position_row, value in enumerate(a_line):
+            neighbour_before = grid[position_lines][position_row - 1] if position_row != 0 else 0
+            neighbour_after = grid[position_lines][position_row + 1] if position_row != len(a_line)-1 else 0
+
+            if neighbour_before == 1 and neighbour_after == 1:
+
+                result[position_lines][position_row] = 1
+            else:
+                result[position_lines][position_row] = 0
+
+
+    return result
+
+
+def test_evolutionary_grid_generator():
+    assert evolutionary_grid_generator([[0, 0, 1]]) == [[0, 0, 0]]
+    assert evolutionary_grid_generator([[1, 1, 1]]) == [[0, 1, 0]]
+    #assert evolutionary_grid_generator()
